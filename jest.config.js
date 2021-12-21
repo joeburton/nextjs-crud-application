3ef57a122f190bfaf4e40dcb7d8d6deda509f45b
Joe Burton <joeburton@gmail.com>
2021-12-21 14:31:05 +0000

    creating dev branch

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,12 @@ module.exports = {
   collectCoverageFrom: ['**/*.{ts,tsx}', '!**/*.d.ts', '!**/node_modules/**'],
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/cypress'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
     '^.+\\.css$': '<rootDir>/cssTransform.js',
   },
-  transformIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: ['/node_modules'],
   moduleNameMapper: {
     '^.+\\.module\\.(css)$': 'identity-obj-proxy',
   },
