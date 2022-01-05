3ceb9c6bf9aa979abbcc2689b96b9815f419f34e
Joe Burton <joeburton@gmail.com>
2022-01-05 11:38:13 +0000

    updated tested component

--- components/TestComponent/TestComponent.test.tsx
+++ components/TestComponent/TestComponent.test.tsx
@@ -10,3 +10,13 @@ test('it should render a test component with the correct props', () => {
   expect(details).toHaveTextContent(/^Force build, 04\/10\/1979, male$/);
   expect(details).toBeInTheDocument();
 });
+
+test('it should render a test component with the correct props', () => {
+  const { getByTestId } = render(
+    <TestComponent name='Test Component' dob='04/10/1979' gender='male' />
+  );
+  const details = getByTestId('personal-details');
+
+  expect(details).toHaveTextContent(/^Test Component, 04\/10\/1979, male$/);
+  expect(details).toBeInTheDocument();
+});
