46f7801adb14a5d0997ac1df0c256753e5fd9145
Joe Burton <joeburton@gmail.com>
2022-01-10 17:01:20 +0000

    adding dynamic pages experiment

--- lib/apollo/queries.ts
+++ lib/apollo/queries.ts
@@ -10,4 +10,22 @@ const GET_DEVELOPERS = gql`
   }
 `;
 
-export { GET_DEVELOPERS };
+const GET_NOTES = gql`
+  query GetNotes {
+    notes {
+      id
+    }
+  }
+`;
+
+const GET_NOTE = gql`
+  query GetNote($id: String) {
+    note(id: $id) {
+      id
+      title
+      note
+    }
+  }
+`;
+
+export { GET_DEVELOPERS, GET_NOTES, GET_NOTE };
---
+++ pages/notes/[id].tsx
@@ -0,0 +1,46 @@
+import { GET_NOTES, GET_NOTE } from '../../lib/apollo/queries';
+import apolloClientNotes from '../../lib/apollo/apolloClientNotes';
+
+const Posts = ({ note }) => {
+  console.log(note);
+  {
+  }
+  return (
+    <>
+      <pre>{JSON.stringify(note, null, 3)}</pre>
+    </>
+  );
+};
+
+export default Posts;
+
+export async function getStaticPaths() {
+  const { data } = await apolloClientNotes.query({
+    query: GET_NOTES,
+  });
+
+  console.log(data);
+
+  const paths = data?.notes.map((note) => ({
+    params: { id: note.id },
+  }));
+
+  console.log(paths);
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps(context) {
+  console.log(context);
+
+  const { data } = await apolloClientNotes.query({
+    query: GET_NOTE,
+    variables: { id: context.params.id },
+  });
+
+  console.log(data);
+
+  return {
+    props: { note: data },
+  };
+}
